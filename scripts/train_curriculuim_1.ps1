98afb07a1681a31177890823b84250a75020d2c6
K-Kikuzumi <kikuzumi.kenta.72a@st.kyoto-u.ac.jp>
2023-03-09 12:01:02 +0900

    [add]train in mdm+icm

---
+++ scripts/train_curriculuim_1.ps1
@@ -0,0 +1,10 @@
+# 前半、故障なしMDM
+python ./train.py -c ewalker_curriculum_first.json -o log/curriculum_first_half_nofail_1
+
+# フリー故障１回目
+# 後半、故障ありICM
+python ./train.py -c ewalker_curriculum_second_free.json -o log/curriculum_second_half_free_1
+
+# ロック故障１回目
+# 後半、故障ありICM
+python ./train.py -c ewalker_curriculum_second_lock.json -o log/curriculum_second_half_lock_1
