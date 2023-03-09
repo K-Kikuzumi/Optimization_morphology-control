4bac287ebac7fdba394a1ae20807637c8373b034
K-Kikuzumi <kikuzumi.kenta.72a@st.kyoto-u.ac.jp>
2023-03-09 21:18:06 +0900

    [add] comment

--- scripts/train_curriculum_1.ps1
+++ scripts/train_curriculum_1.ps1
@@ -6,5 +6,6 @@ python ./train.py -c ewalker_curriculum_first.json -o log/curriculum_first_half_
 python ./train.py -c ewalker_curriculum_second_free.json -o log/curriculum_second_half_free_1
 
 # ロック故障１回目
+# 前半はフリー故障と共有
 # 後半、故障ありICM
 python ./train.py -c ewalker_curriculum_second_lock.json -o log/curriculum_second_half_lock_1
