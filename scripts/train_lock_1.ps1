0824bf9320a95b30edcdbfa83626938c4ec41aa9
K-Kikuzumi <kikuzumi.kenta.72a@st.kyoto-u.ac.jp>
2023-04-03 14:00:18 +0900

    [change] version 2

--- scripts/train_lock_1.ps1
+++ scripts/train_lock_1.ps1
@@ -1,8 +1,8 @@
 # ICM
-python ./train.py -c ewalker_lock_icm.json -o log/thesis_lock_icm_1
+python ./train.py -c ewalker_lock_icm.json -o log/thesis_lock_icm_2
 # (ICM+)MDM
-python ./train.py -c ewalker_lock_icm+mdm.json -o log/thesis_lock_icm+mdm_1
+python ./train.py -c ewalker_lock_icm+mdm.json -o log/thesis_lock_icm+mdm_2
 # MDM
-python ./train.py -c ewalker_lock_mdm.json -o log/thesis_lock_mdm_1
+python ./train.py -c ewalker_lock_mdm.json -o log/thesis_lock_mdm_2
 # (MDM+)ICM
-python ./train.py -c ewalker_lock_mdm+icm.json -o log/thesis_lock_mdm+icm_1
+python ./train.py -c ewalker_lock_mdm+icm.json -o log/thesis_lock_mdm+icm_2
